72e7d946b2ceba672b3504099532008c1ab0be56
mkarpathiota <maria.karpathiotaki@zalando.ie>
2018-01-29 16:02:07 +0000

    Update default ES version

--- src/main/scala/io/grhodes/sbt/elasticsearch/ElasticsearchKeys.scala
+++ src/main/scala/io/grhodes/sbt/elasticsearch/ElasticsearchKeys.scala
@@ -11,7 +11,7 @@ object ElasticsearchKeys {
 
   lazy val elasticsearchClusterName = settingKey[String]("The name of the elasticsearch cluster. Defaults to 'elasticsearch'")
   lazy val elasticsearchUrl = settingKey[Option[String]]("The URL to download elasticsearch from.")
-  lazy val elasticsearchVersion = settingKey[String]("The version of elasticsearch. Defaults to '2.3.4'")
+  lazy val elasticsearchVersion = settingKey[String]("The version of elasticsearch. Defaults to '6.1.2'")
   lazy val elasticsearchHttpPort = settingKey[Int]("Elasticsearch HTTP port. Defaults to '9200'")
   lazy val elasticsearchTcpPort = settingKey[Int]("Elasticsearch TCP port. Defaults to '9300'")
 
--- src/main/scala/io/grhodes/sbt/elasticsearch/ElasticsearchPlugin.scala
+++ src/main/scala/io/grhodes/sbt/elasticsearch/ElasticsearchPlugin.scala
@@ -12,7 +12,7 @@ object ElasticsearchPlugin extends AutoPlugin {
 
   override val projectSettings = Seq(
     elasticsearchClusterName := "elasticsearch",
-    elasticsearchVersion := "2.3.4",
+    elasticsearchVersion := "6.1.2",
     elasticsearchUrl := None,
     elasticsearchHttpPort := 9200,
     elasticsearchTcpPort := 9300,
